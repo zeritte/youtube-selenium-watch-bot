e22bf25124d863bed67c128a55eee4997eae317b
zeritte <zeritte@gmail.com>
2021-03-20 21:29:10 +0300

    watch list of videos

--- main.py
+++ main.py
@@ -4,29 +4,67 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
+video_list = [
+    "https://www.youtube.com/watch?v=QEcSNmDPIEE",
+    "https://www.youtube.com/watch?v=bgadhuBu4yw",
+    "https://www.youtube.com/watch?v=vT5PawWveQM",
+    "https://www.youtube.com/watch?v=2zw4nVVpcR8",
+    "https://www.youtube.com/watch?v=Fk9rW4gYPWU",
+    "https://www.youtube.com/watch?v=11rbvwjKuws",
+    "https://www.youtube.com/watch?v=6MaaOjy1aGU",
+    "https://www.youtube.com/watch?v=GGO-KGIPlPs",
+    "https://www.youtube.com/watch?v=MpbtvRheXK4",
+    "https://www.youtube.com/watch?v=KcCRMe7D5NY",
+    "https://www.youtube.com/watch?v=_vO6ed5s6ik",
+]
+
 
 def main():
     try:
         options = webdriver.ChromeOptions()
         options.add_argument("--incognito")
-        options.add_argument("--headless")
-        options.add_argument("--mute-audio")
+        # options.add_argument("--headless")
+        # options.add_argument("--mute-audio")
 
         driver = webdriver.Chrome(options=options)
-        driver.get("https://www.youtube.com/watch?v=_vO6ed5s6ik")
 
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.ID, "ytp-caption-window-container"))
-        )
-
-        player_button = driver.find_elements_by_css_selector(
+        player_button_css_selector = (
             "#movie_player > div.ytp-cued-thumbnail-overlay > button"
         )
-        player_button = player_button[0]
+        
+        for i in range(0, len(video_list)):
+            driver.get(video_list[i])
+            print("opened the page", driver.title)
 
-        player_button.click()
+            WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.ID, "ytp-caption-window-container"))
+            )
+            print("found video container")
 
-        time.sleep(10)
+            if i == 0:
+                WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located(
+                        (
+                            By.CSS_SELECTOR,
+                            player_button_css_selector,
+                        )
+                    )
+                )
+                print("found play button")
+
+                player_button = driver.find_elements_by_css_selector(
+                    player_button_css_selector
+                )
+                player_button = player_button[0]
+                print("got player button")
+
+                player_button.click()
+                print("clicked player button")
+            else:
+                print("video auto played")
+
+            time.sleep(10)
+
     except Exception as e:
         print(e)
         driver.close()
