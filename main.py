236a91401983fead9bdf7ad4fefd93154e919f52
zeritte <zeritte@gmail.com>
2021-03-20 20:18:53 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+def main():
+    print("hello world")
+
+
+main()
